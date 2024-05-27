6e82b65a6a5addc2255a2e7f54e4cf88fabcb60b
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-05-27 11:48:44 +0530

    add feeds schema

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NUll
+    name TEXT NOT NULL
 );
 
 -- +goose Down
---
+++ sql/schema/003_feeds.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+
+CREATE TABLE feeds(
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    user_id int NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+
+DROP TABLE feeds;
